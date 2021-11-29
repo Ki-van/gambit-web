65d5ed002ef00e4acbd7b5724c5873cca6d2e2a8
Ki-van <kirill.ivanin00@yandex.ru>
2021-11-29 16:08:38 +0300

    insert gallery to model section lg

--- src/components/ModelSectionLg.vue
+++ src/components/ModelSectionLg.vue
@@ -2,10 +2,15 @@
   <div class="mt-4" style="background-color: #e6e6e6;
 border-radius: 50px">
     <div class="row p-lg-5 p-md-3 p-sm-3">
-      <div class="col-lg">
-        <img src="img/plug.png" class="img-fluid " height=auto>
+      <div class="col-lg-6">
+        <VueSlickCarousel v-bind="gallerySettings">
+          <div><img src="img/plug.png" class="img-fluid " height=auto></div>
+          <div><img src="img/plug.png" class="img-fluid" height=auto></div>
+          <div><img src="img/plug.png" class="img-fluid" height=auto></div>
+          <div><img src="img/plug.png" class="img-fluid" height=auto></div>
+        </VueSlickCarousel>
       </div>
-      <div class="col-lg">
+      <div class="col-lg-6 mt-sm-3">
         <h1>Модель 1</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Habitant neque suspendisse diam id in semper
           elementum. Felis, risus donec amet pretium. Sit ac in vel duis. Aliquam a pulvinar sed duis mus duis.</p>
@@ -26,8 +31,24 @@ border-radius: 50px">
 </template>
 
 <script>
+import VueSlickCarousel from "vue-slick-carousel";
 export default {
-  name: "ModelSection"
+  name: "ModelSection",
+  data(){
+    return{
+      gallerySettings: {
+        "dots": true,
+        "dotsClass": "slick-dots custom-dot-class",
+        "edgeFriction": 0.35,
+        "infinite": false,
+        "speed": 500,
+        "arrows": false,
+        "slidesToShow": 1,
+        "slidesToScroll": 1
+      }
+    }
+  },
+  components: {VueSlickCarousel},
 }
 </script>
 
