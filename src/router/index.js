a25d9d7c7514254144fb385b9390e73e1a7f2336
Ki-van <kirill.ivanin00@yandex.ru>
2021-12-25 17:06:41 +0300

    Handle Unauthorized Access

--- src/router/index.js
+++ src/router/index.js
@@ -111,35 +111,14 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  if(to.matched.some(record => record.meta.requiresAuth)) {
-    if (localStorage.getItem('jwt') == null) {
-      next({
-        path: '/login',
-        params: { nextUrl: to.fullPath }
-      })
-    } else {
-      let user = JSON.parse(localStorage.getItem('user'))
-      if(to.matched.some(record => record.meta.is_admin)) {
-        if(user.is_admin == 1){
-          next()
-        }
-        else{
-          next({ name: 'main'})
-        }
-      }else {
-        next()
-      }
-    }
-  } else if(to.matched.some(record => record.meta.guest)) {
-    if(localStorage.getItem('jwt') == null){
-      next()
-    }
-    else{
-      next({ name: 'main'})
-    }
-  }else {
-    next()
+  const authRequired = to.matched.some(record => record.meta.requiresAuth);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    next('/login');
+  } else {
+    next();
   }
-})
+});
 
 export default router
